b8658be4f2454721d6b4926815bad6487f09ddee
leotinoco7 <leofptinoco7@gmail.com>
2022-02-01 21:03:44 -0300

    a

---
+++ Sem 03/Aula 12/funcao.js
@@ -0,0 +1,18 @@
+// FUNÇÃO PARA PEGAR ANO COMPLETO
+
+// VAI RETORNAR A FUNCAO NO CONSOLE LOG
+// new Date().getFullYear;
+// new Date();
+// new Date().getDay;
+
+// SE NAO BOTAR OS () NO FINAL DO CODIGO RETORNA A FUNCAO NO CONSOLE LOG
+let dateY = new Date().getFullYear();
+let dateFull = new Date();
+let dateD = new Date().getDay();
+
+console.log(dateY);
+console.log(dateFull);
+console.log(dateD);
+
+// NUNCA ESQUECER O () NO FINAL SE NAO RETORNA A FUNCAO
+console.log(new Date().getFullYear);
