8706d37fb9073bc94761ed3aa3804dfe99bdcc0e
leotinoco7 <leofptinoco7@gmail.com>
2022-02-05 16:42:45 -0300

    a

--- Sem 03/Aula 15/script.js
+++ Sem 03/Aula 15/script.js
@@ -4,3 +4,5 @@ const pessoa2 = { nome: 'Thiago', empresa: 'Blue' };
 Object.assign(pessoa2, pessoa);
 
 console.log(pessoa2); // { nome: 'Gabriel', empresa: 'Blue', idade: 20 }
+
+console.log('${jogadoresResult[x].typeOf}');
