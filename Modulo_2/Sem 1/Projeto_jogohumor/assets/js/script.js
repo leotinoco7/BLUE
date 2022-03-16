1be5d17104fe321ce1e3348544bf6501da27d82c
leotinoco7 <leofptinoco7@gmail.com>
2022-03-15 22:11:45 -0300

    a

--- Modulo_2/Sem 1/Projeto_jogohumor/assets/js/script.js
+++ Modulo_2/Sem 1/Projeto_jogohumor/assets/js/script.js
@@ -2,22 +2,28 @@ let btnx = document.querySelector('#btn1');
 const ximg = document.querySelector('#imagem');
 const texto = document.querySelector('#state');
 
-btnx.addEventListener('click', () =>{
-    if (btnx.value == 'firstState'){
-    ximg.src = 'assets/img/still.gif'
-    texto.innerText = 'Aguardando ...'
-    btnx.value = 'secondState'
-} else if (btnx.value == 'secondState'){
-    ximg.src = 'assets/img/1122.gif'
-    texto.innerText = 'Carregando 1/3 ...'
-    btnx.value = 'thirdState'
-} else if (btnx.value == 'thirdState'){
-    ximg.src = 'assets/img/2336.gif'
-    texto.innerText = 'Carregando 2/3 ...'
-    btnx.value = 'fourthState'
-} else if (btnx.value == 'fourthState'){
-    ximg.src = 'assets/img/3752.gif'
-    texto.innerText = 'Pronto ! 3/3 ...'
-    btnx.value = 'firstState'
-}
-})
+btnx.addEventListener('click', () => {
+    if (btnx.value == 'firstState') {
+        ximg.src = 'assets/img/still.gif';
+        texto.innerText = 'Aguardando ...';
+        btnx.value = 'secondState';
+    } else if (btnx.value == 'secondState') {
+        ximg.src = 'assets/img/1122.gif';
+        texto.innerText = 'Carregando 1/3 ...';
+        btnx.value = 'thirdState';
+    } else if (btnx.value == 'thirdState') {
+        ximg.src = 'assets/img/2336.gif';
+        texto.innerText = 'Carregando 2/3 ...';
+        btnx.value = 'fourthState';
+    } else if (btnx.value == 'fourthState') {
+        ximg.src = 'assets/img/3752.gif';
+        texto.innerText = 'Pronto ! 3/3 ...';
+        btnx.value = 'firstState';
+    }
+});
+
+document.addEventListener('keyup', function (event) {
+    if (event.keyCode === 81) {
+        document.querySelector('#btn1').click();
+    }
+});
