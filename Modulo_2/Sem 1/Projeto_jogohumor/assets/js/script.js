aa5d4d20df6d8101bceb245570322b989932b64b
leotinoco7 <leofptinoco7@gmail.com>
2022-03-16 00:22:19 -0300

    a

--- Modulo_2/Sem 1/Projeto_jogohumor/assets/js/script.js
+++ Modulo_2/Sem 1/Projeto_jogohumor/assets/js/script.js
@@ -5,19 +5,19 @@ const texto = document.querySelector('#state');
 btnx.addEventListener('click', () => {
     if (btnx.value == 'firstState') {
         ximg.src = 'assets/img/still.gif';
-        texto.innerText = 'Aguardando ...';
+        texto.innerText = 'STATUS: Aguardando ...';
         btnx.value = 'secondState';
     } else if (btnx.value == 'secondState') {
         ximg.src = 'assets/img/1122.gif';
-        texto.innerText = 'Carregando 1/3 ...';
+        texto.innerText = 'STATUS: Carregando 1/3 ...';
         btnx.value = 'thirdState';
     } else if (btnx.value == 'thirdState') {
         ximg.src = 'assets/img/2336.gif';
-        texto.innerText = 'Carregando 2/3 ...';
+        texto.innerText = 'STATUS: Carregando 2/3 ...';
         btnx.value = 'fourthState';
     } else if (btnx.value == 'fourthState') {
         ximg.src = 'assets/img/3752.gif';
-        texto.innerText = 'Pronto ! 3/3 ...';
+        texto.innerText = 'STATUS: Pronto ! 3/3 ...';
         btnx.value = 'firstState';
     }
 });
