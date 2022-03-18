809ab523c819b74d050417a49fc06a38fc14759a
leotinoco7 <leofptinoco7@gmail.com>
2022-03-18 10:46:08 -0300

    a

--- Modulo_2/Sem 3/projeto_pokedex/index.js
+++ Modulo_2/Sem 3/projeto_pokedex/index.js
@@ -3,16 +3,71 @@ const path = require('path');
 const app = express();
 
 const port = 3000;
-let message = '';
+const pokedex = [
+    {
+        id: '4',
+        name: 'Charmander',
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+        desc: 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+        type: 'Fire',
+    },
+    {
+        id: '4',
+        name: 'Charmander',
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+        desc: 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+        type: 'Fire',
+    },
+    {
+        id: '4',
+        name: 'Charmander',
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+        desc: 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+        type: 'Fire',
+    },
+    {
+        id: '4',
+        name: 'Charmander',
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+        desc: 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+        type: 'Fire',
+    },
+    {
+        id: '4',
+        name: 'Charmander',
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+        desc: 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+        type: 'Fire',
+    },
+    {
+        id: '4',
+        name: 'Charmander',
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+        desc: 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+        type: 'Fire',
+    },
+    {
+        id: '4',
+        name: 'Charmander',
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+        desc: 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+        type: 'Fire',
+    },
+    {
+        id: '4',
+        name: 'Charmander',
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+        desc: 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+        type: 'Fire',
+    },
+];
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-    res.render('index', {
-        titulo: 'POKEDEX',
-    });
+app.get('/', (req, res) => {
+    res.render('index', { pokedex });
 });
 
 app.listen(port, () =>
