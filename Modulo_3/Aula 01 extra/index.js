3c9f3428cbe0eb30b339ab3cff78684fd29323ac
leotinoco7 <leofptinoco7@gmail.com>
2022-04-11 23:32:31 -0300

    a

--- Modulo_3/Aula 01 extra/index.js
+++ Modulo_3/Aula 01 extra/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const cors = require('cors');
-const port = 3000;
 const app = express();
 
-app.use(cors());
+const port = 3000;
 
 app.use(express.json());
 
@@ -20,54 +18,15 @@ const playList = [
   },
   {
     id: 3,
-    band: 'The-Corrs',
+    band: 'The Corrs',
     title: 'Breathless',
   },
   {
     id: 4,
     band: 'Racionais Mcs',
-    title: 'Vida loka 2',
+    tittle: 'Vida loka 2',
   },
-  {
-    id: 5,
-    band: 'Criolo',
-    title: 'Padê onã',
-  },
-  {
-    id: 6,
-    band: 'E-40',
-    title: 'Choices',
-  },
-  {
-    id: 7,
-    band: 'Zé ramalho',
-    title: 'Vila do sossego',
-  },
-  {
-    id: 8,
-    band: 'Iron Maiden',
-    title: '666 The Number of the Beast',
-  }
 ];
-
-app.get('/musicas/find-musicas', (req, res) => {
-  res.send(playList);
-});
-
-
-app.get('/musicas/find-musica/:id', (req, res) => {
-  res.send(playList.find((musica) => musica.id == req.params.id));
-});
-
-app.get('/musicas/find-band/:band', (req, res) => {
-    const musicax = playList.filter((musica) => musica.band == req.params.band);
-    
-    if (musicax !== [] || musicax !== null){
-        res.send(musicax);
-    }else {
-      res.send("musica não encontrada");
-    }
-  });
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
