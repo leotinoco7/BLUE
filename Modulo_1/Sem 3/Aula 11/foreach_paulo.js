118819fa34577c3321274b8e195a136ad1864b0c
leotinoco7 <leofptinoco7@gmail.com>
2022-05-02 19:59:12 -0300

    a

--- Modulo_1/Sem 3/Aula 11/foreach_paulo.js
+++ Modulo_1/Sem 3/Aula 11/foreach_paulo.js
@@ -1,45 +1,45 @@
-const numeros = [10, 20, 30];
+// const numeros = [10, 20, 30];
 
-for (const numero of numeros) {
-    conmsole.log({ numero });
-}
+// for (const numero of numeros) {
+//     conmsole.log({ numero });
+// }
 
-numeros.forEach();
+// numeros.forEach();
 
-// ----------------------------------------------------
+// // ----------------------------------------------------
 
-const somaArrow = (a, b) => (a, b);
+// const somaArrow = (a, b) => (a, b);
 
-// VALOR, INDICE, LISTA
-function minhaFuncao(a, b, c) {
-    console.log(a, b, c);
-}
+// // VALOR, INDICE, LISTA
+// function minhaFuncao(a, b, c) {
+//     console.log(a, b, c);
+// }
 
-minhaFuncao(1, 2, 3);
+// minhaFuncao(1, 2, 3);
 
-const numeros = [10, 20, 30];
+// const numeros = [10, 20, 30];
 
-numeros.forEach(minhaFuncao);
+// numeros.forEach(minhaFuncao);
 
-numeros.forEach((valor, indice, lista) =>
-    console.log({ valor, indice, lista }),
-);
+// numeros.forEach((valor, indice, lista) =>
+//     console.log({ valor, indice, lista }),
+// );
 
-// É IGUAL A :
+// // É IGUAL A :
 
-numeros.forEach(console.log);
+// numeros.forEach(console.log);
 
-// ---------------------------------
+// // ---------------------------------
 
-const numeros = [undefined, null, 'Paulo', '', 'Salvatore'];
-for (const nome of nomes) {
-    if (nome !== undefined && nome !== null && nome != '') {
-        nomesLimpos.push(nome);
-    }
-}
+// const numeros = [undefined, null, 'Paulo', '', 'Salvatore'];
+// for (const nome of nomes) {
+//     if (nome !== undefined && nome !== null && nome != '') {
+//         nomesLimpos.push(nome);
+//     }
+// }
 
-console.log(nomesLimpos);
+// console.log(nomesLimpos);
 
-const nomesLimpos2 = nomes.filter(Boolean);
+// const nomesLimpos2 = nomes.filter(Boolean);
 
-console.log({ nomesLimpos2 });
+// console.log({ nomesLimpos2 });
