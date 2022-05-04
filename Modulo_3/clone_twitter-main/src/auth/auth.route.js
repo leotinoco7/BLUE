56199528b15d9c9eaae328b27f886c8471570d37
leotinoco7 <leofptinoco7@gmail.com>
2022-05-04 20:44:50 -0300

    a

--- Modulo_3/clone_twitter-main/src/auth/auth.route.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const authController = require("./auth.controller");
-
-router.post("/login", authController.loginController);
-
-module.exports = router;
---
+++ Modulo_3/spa-twitterclone-main/README.md
@@ -0,0 +1,1 @@
+# spa-twitterclone
